38c22779988db10ec2154ded08d556d01442862c
Tim <dalytimdaly@gmail.com>
2022-08-09 16:32:13 -0600

    commit tree 03df356101e4b2cd43ad8a6530c73c49f277be51

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.querySelector('button')
+
 function addingEventListener() {
+    input.addEventListener('click', function(event) {
+        return "clicked"
+    });
 }
+
+addingEventListener();
